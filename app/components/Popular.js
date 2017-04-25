14153e6fa770178a6b0e655803f6cd3c2c206c80
catacid <Programista@Mac.local>
2017-04-25 13:55:59 +0200

    temp commit cause i might not be able to work today T.T

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,6 @@
 var React = require('react');
 var PropTypes = require('prop-types');
-
+var api = require('../utils/api');
 
 
 const SelectLanguage = (props) => {
@@ -25,27 +25,57 @@ SelectLanguage.propTypes = {
     onSelect: PropTypes.func.isRequired
 }
 
+function RepoGrid(props) {
+    return (
+        <ul className='popular-list'>
+            {props.repos.map((repo, index) =>){
+                <li key={repo.name} className='popular-item'>
+
+                </li>
+            }}
+        </ul>
+    )
+}
 
 
 class Popular extends React.Component{
     constructor (props) {
         super(props);
         this.state = {
-            selectedLanguage: 'All'
+            selectedLanguage: 'All',
+            repos : null,
+
         };
 
         this.updateLanguage = this.updateLanguage.bind(this);
     }
+    componentDidMount() {
+        this.updateLanguage(this.state.selectedLanguage);
+    }
     updateLanguage(lang) {
         console.log('selected: '+lang);
-        this.setState(() => { return {selectedLanguage: lang}});
+        this.setState(() => {
+            return {
+                selectedLanguage: lang,
+                repos: null
+            }
+        });
+        api.fetchPopularRepos(lang)
+            .then(repos => {
+                this.setState(() => {
+                    return {
+                        repos: repos
+                    }
+                })
+            });
     }
     render() {
         return (
             <div>
                 <SelectLanguage
                     selectedLanguage={this.state.selectedLanguage}
-                    onSelect={this.updateLanguage}/>
+                    onSelect={this.updateLanguage} />
+                <RepoGrid repos={this.state.repos} />
             </div>
         )
       }
