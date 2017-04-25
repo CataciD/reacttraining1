70a07d4e1b2e4856526edb54258c154b96d3079e
catacid <Programista@Mac.local>
2017-04-25 12:06:46 +0200

    Stateless functional components update

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,4 +1,31 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+
+
+
+const SelectLanguage = (props) => {
+    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+    return(
+        <ul className='languages'>
+          {languages.map((lang) => {
+            return(
+              <li
+                  style={lang === props.selectedLanguage ? { color: '#d0021b'} : null}
+                  onClick={props.onSelect.bind(null, lang)}
+                  key={lang}>
+                      {lang}
+              </li>
+            )
+        })}
+        </ul>
+    )
+}
+SelectLanguage.propTypes = {
+    selectedLanguage : PropTypes.string.isRequired,
+    onSelect: PropTypes.func.isRequired
+}
+
+
 
 class Popular extends React.Component{
     constructor (props) {
@@ -14,22 +41,17 @@ class Popular extends React.Component{
         this.setState(() => { return {selectedLanguage: lang}});
     }
     render() {
-        var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
         return (
-          <ul className='languages'>
-            {languages.map((lang) => {
-              return(
-                <li
-                    style={lang === this.state.selectedLanguage ? { color: '#d0021b'} : null}
-                    onClick={this.updateLanguage.bind(null, lang)}
-                    key={lang}>
-                        {lang}
-                </li>
-              )
-          })}
-          </ul>
+            <div>
+                <SelectLanguage
+                    selectedLanguage={this.state.selectedLanguage}
+                    onSelect={this.updateLanguage}/>
+            </div>
         )
       }
     }
+
+
+
 
 module.exports = Popular;
